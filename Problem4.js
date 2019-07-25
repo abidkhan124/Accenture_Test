35502601235584d9b0db9a16c25338d47164d3a3
abidkhan124 <53274949+abidkhan124@users.noreply.github.com>
2019-07-25 17:25:59 +0530

    Update Problem4.js

--- Problem4.js
+++ Problem4.js
@@ -2,7 +2,7 @@
 
 // Using split and reverse
 
-function reverseString(str) {
+function stringReverse(str) {
    const arr = str.split(' ');
   
  let output = [];
@@ -18,7 +18,28 @@ output.push(element.split('').reverse().join(''));
 return output.join(' ');
 }
 
-console.log(reverseString('Hello World!'));
+console.log(stringReverse('Hello World!'));
 
 //Without using split and reverse and in O(1) space (since strings are immutable, assume that the input is a character array).
+
+var input = 'Hello World';
+var inputStr = Array.from(input);
+function stringReverse(str){
+    var first = 0;
+    for(var i = 0; i <= str.length; i++){
+        if((str[i] == ' ') || (i == str.length)){
+        var last = i-1;
+        while(first < last){
+            [str[first],str[last]] = [str[last],str[first]];
+            first++;
+            last--;
+            
+        }
+        first = i+1;
+    }
+    }
+    return str;
+}
+
+console.log(stringReverse(inputStr));
 
